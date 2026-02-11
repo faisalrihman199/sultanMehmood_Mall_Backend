406e3d4404ed0bf4925077cece2d5e53f209e0db
faisalrihman199 <129194773+faisalrihman199@users.noreply.github.com>
2026-02-11 23:47:00 +0500

    DB SSL is added

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+const isSSL = process.env.DB_SSL === 'true';
+
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'sultan_mehmood_mall',
   process.env.DB_USER || 'postgres',
@@ -10,6 +12,16 @@ const sequelize = new Sequelize(
     port: parseInt(process.env.DB_PORT || '5432'),
     dialect: 'postgres',
     logging: false,
+
+    dialectOptions: isSSL
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+
     pool: {
       max: 10,
       min: 0,
